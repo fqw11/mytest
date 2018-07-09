9bd43b2a2a194e03528e1b24485c6d8b048553aa
fqw <fqw@itcast.cn>
2018-07-09 15:30:38 +0800

    更新3

--- test-01/src/cn/itcast/test/dome01.java
+++ test-01/src/cn/itcast/test/dome01.java
@@ -4,6 +4,8 @@ public class dome01 {
 public static void main(String[] args) {
 	System.out.println("我是Java开发工程师");
 	System.out.println("我是前端开发工程师");
-
+	System.out.println("我是J212发工程师");
+	System.out.print1发工程师");
+	
 }
 }
